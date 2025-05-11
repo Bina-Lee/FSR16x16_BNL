cca786cef669ed99847e0d076fb90f389e83131b
이진성 <awwdde22ljs@gmail.com>
2025-05-11 17:09:41 +0900

    fix type name error
    fix type name error

--- src/FSR16x16_BNL.h
+++ src/FSR16x16_BNL.h
@@ -70,8 +70,8 @@ class FSR16x16_BNL {
 
         void setRowIdxMap(int* rowIdxMap);
         void setColIdxMap(int* colIdxMap);
-        void setRowIdxMap(mapType mapT);
-        void setColIdxMap(mapType mapT);
+        void setRowIdxMap(mapIdxType mapT);
+        void setColIdxMap(mapIdxType mapT);
 
     private:
         static const int SIZE = 16;
